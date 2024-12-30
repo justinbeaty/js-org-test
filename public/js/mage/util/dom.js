83e9266da04eed3df9eafdc90bdd8d5db6de2421
Justin Beaty <justin.beaty@gmail.com>
2024-12-30 09:44:22 -0800

    jsdoc test

--- public/js/mage/util/dom.js
+++ public/js/mage/util/dom.js
@@ -1,14 +1,17 @@
 /**
  * Maho
  *
- * @category    Mage
- * @package     Mage_Util
  * @copyright   Copyright (c) 2024 Maho (https://mahocommerce.com)
  * @license     https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
-var Mage = Mage ?? {};
+/** @namespace Mage */
+Mage ??= {};
+
+/** @namespace Mage.Util */
 Mage.Util ??= {};
+
+/** @namespace Mage.Util.Dom */
 Mage.Util.Dom ??= {};
 
 /**
